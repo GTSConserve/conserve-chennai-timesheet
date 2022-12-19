9e1879b5a71d59c06b28a88592f743c0338a6a24
raghu12123 <raghu12123@gmail.com>
2022-12-19 21:02:04 +0530

    check

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\Chennai\TimeSheetController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/dashboard', function () {
     return view('auth.login');
 });
 
@@ -38,91 +38,91 @@ Route::group(['prefix' => 'super-admin', 'middleware' => [
     Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('super-admin-project-timesheet-view');
 });
 // =======================================<--Location Head-->=========================================================
-Route::group(['prefix' => 'location-head', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'locationHead'
-], 'namespace' => 'Chennai'], function () {
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('location-head-dashboard');
-    Route::get('/add-employee', [EmployeeController::class, 'add_employee'])->name('location-head-add-employee');
-    Route::get('/all-employee', [EmployeeController::class, 'all_employee'])->name('location-head-all-employee');
-    Route::get('/activity', [ProjectController::class, 'add_activity'])->name('location-head-activity');
-    Route::get('/task', [ProjectController::class, 'add_task'])->name('location-head-task');
-    Route::get('/project', [ProjectController::class, 'add_project'])->name('location-head-project');
-    Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('location-head-project-assign');
-    Route::get('/add-timesheet', [TimeSheetController::class, 'add_timesheet'])->name('location-head-add-timesheet');
-    Route::get('/aproved-timesheet', [TimeSheetController::class, 'aproved_timesheet'])->name('location-head-aproved-timesheet');
-    Route::get('/pending-timesheet', [TimeSheetController::class, 'pending_timesheet'])->name('location-head-pending-timesheet');
-    Route::get('/approval-timesheet', [TimeSheetController::class, 'approval_timesheet'])->name('location-head-approval-timesheet');
-    // Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('location-head-project-timesheet-view');
+// Route::group(['prefix' => 'location-head', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'locationHead'
+// ], 'namespace' => 'Chennai'], function () {
+//     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('location-head-dashboard');
+//     Route::get('/add-employee', [EmployeeController::class, 'add_employee'])->name('location-head-add-employee');
+//     Route::get('/all-employee', [EmployeeController::class, 'all_employee'])->name('location-head-all-employee');
+//     Route::get('/activity', [ProjectController::class, 'add_activity'])->name('location-head-activity');
+//     Route::get('/task', [ProjectController::class, 'add_task'])->name('location-head-task');
+//     Route::get('/project', [ProjectController::class, 'add_project'])->name('location-head-project');
+//     Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('location-head-project-assign');
+//     Route::get('/add-timesheet', [TimeSheetController::class, 'add_timesheet'])->name('location-head-add-timesheet');
+//     Route::get('/aproved-timesheet', [TimeSheetController::class, 'aproved_timesheet'])->name('location-head-aproved-timesheet');
+//     Route::get('/pending-timesheet', [TimeSheetController::class, 'pending_timesheet'])->name('location-head-pending-timesheet');
+//     Route::get('/approval-timesheet', [TimeSheetController::class, 'approval_timesheet'])->name('location-head-approval-timesheet');
+//     // Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('location-head-project-timesheet-view');
 
-});
-//
-Route::group(['prefix' => 'project-manager-head', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'projectManagerHead'
-], 'namespace' => 'Chennai'], function () {
-    Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('project-manager-headdashboard');
-    Route::get('/activity', [ProjectController::class, 'add_activity'])->name('activity');
-    Route::get('/task', [ProjectController::class, 'add_task'])->name('task');
-    Route::get('/project', [ProjectController::class, 'add_project'])->name('project');
-    Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
-    Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
-});
+// });
+// //
+// Route::group(['prefix' => 'project-manager-head', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'projectManagerHead'
+// ], 'namespace' => 'Chennai'], function () {
+//     Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('project-manager-headdashboard');
+//     Route::get('/activity', [ProjectController::class, 'add_activity'])->name('activity');
+//     Route::get('/task', [ProjectController::class, 'add_task'])->name('task');
+//     Route::get('/project', [ProjectController::class, 'add_project'])->name('project');
+//     Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
+//     Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
+// });
 
-Route::group(['prefix' => 'project-manager', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'projectManager'
-], 'namespace' => 'Chennai'], function () {
-    Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('project-manager-dashboard');
-    Route::get('/task', [ProjectController::class, 'add_task'])->name('task');
-    Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-manager-project-assign');
-    Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-manager-project-timesheet-add');
-    Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-manager-project-timesheet-view');
-});
-Route::group(['prefix' => 'project-lead', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'projectLead'
-], 'namespace' => 'Chennai'], function () {
-    Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('project-lead-dashboard');
-    Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-assign');
-    Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
-    Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
-});
+// Route::group(['prefix' => 'project-manager', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'projectManager'
+// ], 'namespace' => 'Chennai'], function () {
+//     Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('project-manager-dashboard');
+//     Route::get('/task', [ProjectController::class, 'add_task'])->name('task');
+//     Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-manager-project-assign');
+//     Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-manager-project-timesheet-add');
+//     Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-manager-project-timesheet-view');
+// });
+// Route::group(['prefix' => 'project-lead', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'projectLead'
+// ], 'namespace' => 'Chennai'], function () {
+//     Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('project-lead-dashboard');
+//     Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-assign');
+//     Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
+//     Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
+// });
 
-Route::group(['prefix' => 'team-lead', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'teamLead'
-], 'namespace' => 'Chennai'], function () {
-    Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('team-lead-dashboard');
-    Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-assign');
-    Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
-    Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
-});
+// Route::group(['prefix' => 'team-lead', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'teamLead'
+// ], 'namespace' => 'Chennai'], function () {
+//     Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('team-lead-dashboard');
+//     Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-assign');
+//     Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
+//     Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
+// });
 
-Route::group(['prefix' => 'employee', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'employee'
-], 'namespace' => 'Chennai'], function () {
-    Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('employee-dashboard');
-    Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-assign');
-    Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
-    Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
-});
+// Route::group(['prefix' => 'employee', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'employee'
+// ], 'namespace' => 'Chennai'], function () {
+//     Route::get('/dashboard', [EmployeeController::class, 'add_employee'])->name('employee-dashboard');
+//     Route::get('/project-assign', [ProjectController::class, 'project_assign'])->name('project-assign');
+//     Route::get('/project-timesheet-add', [ProjectController::class, 'timesheetadd'])->name('project-timesheet-add');
+//     Route::get('/project-timesheet-view', [ProjectController::class, 'timesheetview'])->name('project-timesheet-view');
+// });
 
 
 
-Route::group(['prefix' => 'location-head', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'locationHead'
-], 'namespace' => 'Chennai'], function () {
-});
-Route::group(['prefix' => 'hr-head', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'hrHead'
-], 'namespace' => 'Chennai'], function () {
-});
-Route::group(['prefix' => 'admin', 'middleware' => [
-    'auth:sanctum', config('jetstream.auth_session'), 'verified',
-    'admin'
-], 'namespace' => 'Chennai'], function () {
-});
+// Route::group(['prefix' => 'location-head', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'locationHead'
+// ], 'namespace' => 'Chennai'], function () {
+// });
+// Route::group(['prefix' => 'hr-head', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'hrHead'
+// ], 'namespace' => 'Chennai'], function () {
+// });
+// Route::group(['prefix' => 'admin', 'middleware' => [
+//     'auth:sanctum', config('jetstream.auth_session'), 'verified',
+//     'admin'
+// ], 'namespace' => 'Chennai'], function () {
+// });
