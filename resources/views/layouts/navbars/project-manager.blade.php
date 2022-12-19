18a6bd50659b6c0257a52a7daaaa3e2895e55c0c
raghu12123 <raghu12123@gmail.com>
2022-12-19 10:38:40 +0530

    rout and navbar flow

--- resources/views/layouts/navbars/project-manager.blade.php
+++ resources/views/layouts/navbars/project-manager.blade.php
@@ -21,33 +21,7 @@
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
-                <ul class="collapse submenu list-unstyled" id="dashboard" data-parent="#topAccordion">
-                    <li class="active">
-                        <a href="index.html"> Analytics </a>
-                    </li>
-                    <li>
-                        <a href="index2.html"> Sales </a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="menu single-menu">
-                <a href="#app" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cpu"><rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect><rect x="9" y="9" width="6" height="6"></rect><line x1="9" y1="1" x2="9" y2="4"></line><line x1="15" y1="1" x2="15" y2="4"></line><line x1="9" y1="20" x2="9" y2="23"></line><line x1="15" y1="20" x2="15" y2="23"></line><line x1="20" y1="9" x2="23" y2="9"></line><line x1="20" y1="14" x2="23" y2="14"></line><line x1="1" y1="9" x2="4" y2="9"></line><line x1="1" y1="14" x2="4" y2="14"></line></svg>
-                        <span><b>Employee</b></span>
-                    </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
-                </a>
-                <ul class="collapse submenu list-unstyled" id="app" data-parent="#topAccordion">
-                    <li>
-                        <a href="{{route('add-employee')}}"><b>Add Emoloyee</b></a>
-                    </li>
-                    <li>
-                        <a href="{{route('all-employee')}}"><b>All Emoloyee</b></a>
-                    </li>
-                   
-                </ul>
+          
             </li>
 
             <li class="menu single-menu">
@@ -59,12 +33,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
                 <ul class="collapse submenu list-unstyled" id="components" data-parent="#topAccordion">
-                    <li>
-                        <a href="{{route('activity')}}"><b>Activity</b></a>
-                    </li>
-                    <li>
-                        <a href="{{route('project')}}"><b>Project</b></a>
-                    </li>
+                   
                     <li>
                         <a href="{{route('task')}}"><b>Task</b></a>
                     </li>
@@ -79,12 +48,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
                 <ul class="collapse submenu list-unstyled" id="components" data-parent="#topAccordion">
-                    <li>
-                        <a href="{{route('activity')}}"><b>Activity</b></a>
-                    </li>
-                    <li>
-                        <a href="{{route('project')}}"><b>Project</b></a>
-                    </li>
+                  
                     <li>
                         <a href="{{route('task')}}"><b>Task</b></a>
                     </li>
--- resources/views/layouts/navbars/super-admin.blade.php
+++ resources/views/layouts/navbars/super-admin.blade.php
@@ -21,14 +21,7 @@
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
-                <ul class="collapse submenu list-unstyled" id="dashboard" data-parent="#topAccordion">
-                    <li class="active">
-                        <a href="index.html"> Analytics </a>
-                    </li>
-                    <li>
-                        <a href="index2.html"> Sales </a>
-                    </li>
-                </ul>
+               
             </li>
 
             <li class="menu single-menu">
